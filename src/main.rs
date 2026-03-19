f3bd310d7d0eaea58f1b7c007e168506a6f02e2b
kushal karamchandani <kushal.karamchandani@nordakademie.de>
2026-03-19 15:34:12 +0100

    loaded player 1 frame

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use rusty_engine::prelude::*;
+
+#[derive(Resource)]
+struct GameState {
+    high_score:u32,
+    current_score:u32,
+    enemy_labels: Vec<String>,
+    spawn_timer: Timer
+}
+
+impl Default for GameState {
+    fn default() -> Self {    
+        Self{
+            high_score:0,
+            current_score:0,
+            enemy_labels: Vec::new(),
+            spawn_timer: Timer::from_seconds(1.0, TimerMode::Once)
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    let mut game = Game::new();
+
+    let player1 = game.add_sprite("player 1", SpritePreset::RacingCarRed);
+    player1.translation = Vec2::new(-300.0, 0.0);
+
+    game.add_logic(game_logic);
+
+    game.run(GameState::default());
+}
+
+
+fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
+
 }
